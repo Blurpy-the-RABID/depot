c287d03fabc99351fc9fd33eb6ff222c948c46bd
Vincent Fantini <blurpy_the_rabid@hotmail.com>
2013-02-20 15:43:30 -0500

    Initial Commit
    I've proceeded through Section 6.1 of the Agile Web Development with
    Rails book (4th ed.), and have created the Rails Application for my
    Depot app.  I've created the database, generated the scaffolding, and
    applied the migration as per the instructions.  I've also created a
    single product so as to populate my Products listing with something.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '943f5f1f952ff4863c4ddfe9063f985eafab75b00521e89afc8fd80a6c3cda5b038dd18b740bbce138c60fa3f4a3cc8639268db7ed62efba03d2ddfd44fd711a'
