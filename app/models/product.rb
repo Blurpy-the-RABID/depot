bb6bd21cbfaba6409d1319e86b6d8451554496b1
Vincent Fantini <blurpy_the_rabid@hotmail.com>
2013-02-26 12:01:14 -0500

    Created line_items table; connected Products to Carts.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -7,4 +11,16 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  
+  private
+      
+    # Ensure that there are no line items referencing this product.
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
